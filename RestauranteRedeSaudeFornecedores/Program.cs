2925a2ef6e83492d0ab3f7a93990f0f48c1eedbb
contato <contato@p4x.com.br>
2025-02-24 11:12:35 -0300

    ok

--- RestauranteRedeSaudeFornecedores/Program.cs
+++ RestauranteRedeSaudeFornecedores/Program.cs
@@ -1,3 +1,6 @@
+using Data.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -9,9 +12,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
