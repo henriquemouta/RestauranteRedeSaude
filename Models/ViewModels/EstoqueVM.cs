4929ced0983c66c78a12739f2c2e83c9ff796e8d
contato <contato@p4x.com.br>
2025-02-24 11:54:09 -0300

    Ok

---
+++ Models/ViewModels/EstoqueVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class EstoqueVM
+    {
+        public int ID { get; set; }  
+
+        public string Nome { get; set; }  
+
+        public int Quantidade { get; set; }  
+
+        public decimal PrecoUnitario { get; set; }  
+
+        public string Categoria { get; set; }  
+
+    }
+}
---
+++ Models/ViewModels/FuncionarioVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class FuncionarioVM
+    {
+        public int ID { get; set; }  
+
+        public string Nome { get; set; }  
+
+        public string Cargo { get; set; }  
+
+        public string Telefone { get; set; }  
+    }
+}
